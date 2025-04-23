072f82a5fbf36157e1ca545afb2fade4a466ae15
jimmy <jimoseipeace2bb@gmail.com>
2025-04-22 22:52:17 -0500

    sql connection done. Close #1

---
+++ src/main/java/com/jimdev/ems/model/Employee.java
@@ -0,0 +1,23 @@
+package com.jimdev.ems.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
